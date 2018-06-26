3f303dd7474d496768369160de4c831cca7f9548
sunghoon9@naver.com <root@bob.obenef.com>
2018-06-26 15:19:07 +0900

    Update:2018-06-26

--- sites/all/themes/custom/bob_default/js/prototype.js
+++ sites/all/themes/custom/bob_default/js/prototype.js
@@ -1,26 +1,25 @@
 // Array - Shuffle //
-Array.prototype.shuffle = function() {
-	var targetArray = this;
-	var element = null;
-	var length = targetArray.length;
-	var shuffleArray = new Array();
-	var index = 0;
+function Addon() {
+	this.shuffle = function(array) {
+		var targetArray = array;
+		var element = null;
+		var length = targetArray.length;
+		var shuffleArray = new Array();
+		var index = 0;
 
-	while(length > 0) {
-		index = Math.floor(Math.random() * length);
-		shuffleArray.push(targetArray[index]);
-		targetArray.splice(index, 1);
+		while(length > 0) {
+			index = Math.floor(Math.random() * length);
+			shuffleArray.push(targetArray[index]);
+			targetArray.splice(index, 1);
 
-		length = targetArray.length;
+			length = targetArray.length;
+		}
+		return shuffleArray;
 	}
+	this.replaceAll = function(str, searchValue, newValue) {
+		var str = str;
+		var replaceValue = str.split(searchValue).join(newValue);
 
-	return shuffleArray;
-};
-
-// String - replaceAll //
-String.prototype.replaceAll = function(searchValue, newValue) {
-	var str = this.toString();
-	var replaceValue = str.split(searchValue).join(newValue);
-
-	return replaceValue;
+		return replaceValue;
+	}
 }
--- sites/all/themes/custom/bob_default/js/scripts.js
+++ sites/all/themes/custom/bob_default/js/scripts.js
@@ -1,4 +1,4 @@
-/* 2018-06-25 */
+/* 2018-06-26 */
 jQuery(document).ready(function() {
     var color_value = Math.floor(4 * Math.random()), $body = jQuery("body"), $main_container = jQuery("#main_container");
     $body.addClass([ "ultra_violet", "greenery", "rose_quartz", "serenity" ][color_value]), 
@@ -9,17 +9,7 @@ jQuery(document).ready(function() {
         (void 0).removeClass("opened"), (void 0).addClass("closed"), $main_container.removeClass("opened"), 
         $main_container.addClass("closed"));
     });
-}), Array.prototype.shuffle = function() {
-    for (var length = this.length, shuffleArray = new Array(), index = 0; 0 < length; ) {
-        index = Math.floor(Math.random() * (length - 1)), shuffleArray.push(this[index]), 
-        this.splice(index, 1), console.log("---------------"), console.log("length = " + length), 
-        console.log("index = " + index), console.log(this), console.log(shuffleArray), console.log("---------------"), 
-        length = this.length;
-    }
-    return shuffleArray;
-}, String.prototype.replaceAll = function(searchValue, newValue) {
-    return this.toString().split(searchValue).join(newValue);
-}, jQuery(document).ready(function(e) {
+}), jQuery(document).ready(function(e) {
     jQuery(window).outerWidth();
     var window_height = jQuery(window).outerHeight(), wrapper_offset = jQuery("#wrapper").offset(), wrapper_top = wrapper_offset.top, wrapper_height = window_height - wrapper_top;
     jQuery("header").outerHeight(), jQuery("footer").outerHeight();
